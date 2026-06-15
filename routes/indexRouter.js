101983d7f523a28c9a77bec174e8a0706cbd1925
Matt <myatthuta1835@gmail.com>
2026-06-15 23:30:28 +0630

    Initial commit

---
+++ routes/indexRouter.js
@@ -0,0 +1,44 @@
+const { Router } = require("express");
+
+const indexRouter = Router();
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+]
+
+indexRouter.get("/", (req, res) => {
+    res.render("index", {
+        title: "Mini Messageboard",
+        messages
+    })
+})
+
+indexRouter.get("/new", (req, res) => {
+    res.render("form");
+})
+
+indexRouter.post("/new", (req, res) => {
+    messages.push({
+        text: req.body.messageText,
+        user: req.body.author,
+        added: new Date()
+    });
+
+    res.redirect("/");
+})
+
+indexRouter.get("/message/:id", (req, res) => {
+    const message = messages[req.params.id];
+    res.render("message", { message });
+})
+
+module.exports = { indexRouter };
